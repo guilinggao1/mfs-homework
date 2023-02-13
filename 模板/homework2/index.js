7fd2131a39cf02b47a10f4d4877271c6efb9450c
guilinggao1 <107974448+guilinggao1@users.noreply.github.com>
2023-02-13 16:11:02 +0800

    Add files via upload

---
+++ 模板/homework2/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post("/usernameExist", function (req, res, next) {
+  res.json({
+    exist: "marktest" == req.body.username
+  })
+})
+
+router.post("/", function (req, res, next) {
+  res.send({ title: "成功" })
+})
+module.exports = router;
