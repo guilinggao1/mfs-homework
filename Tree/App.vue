d019f3efb01a6c47c7f56f950954523881d1ad51
guilinggao1 <107974448+guilinggao1@users.noreply.github.com>
2023-02-21 21:34:15 +0800

    Create App.vue

---
+++ Tree/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul>
+    <TreeItem class="item" :model="treeData"></TreeItem>
+  </ul>
+</template>
+
+<script>
+import TreeItem from './components/TreeItem.vue'
+
+const treeData = {
+  name: 'My Tree',
+  children: [
+    { name: 'hello' },
+    { name: 'wat' },
+    {
+      name: 'child folder',
+      children: [
+        {
+          name: 'child folder',
+          children: [{ name: 'hello' }, { name: 'wat' }]
+        },
+        { name: 'hello' },
+        { name: 'wat' },
+        {
+          name: 'child folder',
+          children: [{ name: 'hello' }, { name: 'wat' }]
+        }
+      ]
+    }
+  ]
+}
+
+export default {
+  components: {
+    TreeItem
+  },
+  data() {
+    return {
+      treeData
+    }
+  }
+}
+</script>
+
+<style>
+.item {
+  cursor: pointer;
+  line-height: 1.5;
+}
+
+.bold {
+  font-weight: bold;
+}
+</style>
