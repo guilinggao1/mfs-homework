33172aa818c3b5ec3a12238f3d36503c46de5d62
guilinggao1 <107974448+guilinggao1@users.noreply.github.com>
2023-02-23 15:58:06 +0800

    Add files via upload

---
+++ 单组件文件/Btn.vue
@@ -0,0 +1,28 @@
+<template>
+    <button @click="btnClick()">{{ count }}</button>
+</template>
+<script>
+export default {
+    name: 'my-btn',
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        btnClick: function () {
+            this.count++
+            this.$emit('btnClick')
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* button {
+    background-color: rgb(23, 173, 83);
+    color: rgb(233, 233, 242);
+    border: none;
+
+} */
+</style>
---
+++ 单组件文件/Test.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div>{{ sum }}</div>
+        <my-btn @btnClick="btnClick"></my-btn>
+        <my-btn @btnClick="btnClick"></my-btn>
+        <my-btn @btnClick="btnClick"></my-btn>
+        <p><img :src="pic"></p>
+    </div>
+</template>
+<script>
+import MyBtn from './Btn.vue'
+import pic from '../assets/logo.png'
+
+export default {
+    name: 'test',
+    data() {
+        return {
+            sum: 0,
+            pic
+        }
+    },
+    methods: {
+        btnClick: function () {
+            this.sum++
+        }
+    },
+    components: { MyBtn }
+}
+</script>
+
+<style scoped>
+img {
+    width: 200px;
+    height: 200px;
+}
+</style>
