5244e1104de942fdddc97bbb9de26cd8dac3bd8b
guilinggao1 <107974448+guilinggao1@users.noreply.github.com>
2023-02-24 21:42:58 +0800

    Add files via upload

---
+++ Vue-router/homework1/components/VLink.vue
@@ -0,0 +1,34 @@
+<template>
+    <a @click="onClick" :href="href" :class="{ active: isActive }">
+        <slot></slot>
+    </a>
+</template>
+<script>
+export default {
+    props: {
+        href: {
+            type: String,
+            required: true,
+        }
+    },
+    // name: 'vlink',
+    methods: {
+        onClick(e) {
+            e.preventDefault()
+            let path = this.href
+            this.$root.currentRoute = path
+            window.history.pushState(null, null, path)
+        }
+    },
+    computed: {
+        isActive: function () {
+            return this.$root.currentRoute == this.href
+        }
+    }
+}
+</script>
+<style >
+.active {
+    background-color: aquamarine;
+}
+</style>
