8399a3b7d025470ea4fe2ac96276079760b12641
guilinggao1 <107974448+guilinggao1@users.noreply.github.com>
2023-02-24 21:44:43 +0800

    Create About.vue

---
+++ Vue-router/homework2/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <!-- <router-link to='/'>home</router-link>
+        <router-link to='/about'>about</router-link> -->
+        <p>about page</p>
+    </div>
+</template>
+<script>
+// import Vlink from './VLink.vue'
+export default {
+    // components: { Vlink }
+}
+</script>
+<style ></style>
