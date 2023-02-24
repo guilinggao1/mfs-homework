42a162fdd5b6e21b622504d7cd98208f432405b2
guilinggao1 <107974448+guilinggao1@users.noreply.github.com>
2023-02-24 21:33:42 +0800

    Create About.vue

---
+++ vue-blog/pages/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="about">
+        <h2>关于</h2>
+        <div class="logo"><img src="../assets/logo.png" alt=""></div>
+        <div class="content">内容</div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'about',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<style>
+.about {
+    text-align: center;
+}
+</style>
